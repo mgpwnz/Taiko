7f69cd207c2538b2524c226aad3ba0c7e4da62ff
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-15 18:51:35 +0200

    updt

--- holeski.sh
+++ holeski.sh
@@ -34,34 +34,35 @@ else
     sudo usermod -aG sudo holesky
 fi
 # Змінити користувача
-su - holesky
+su -l holesky -c '
 
-# Переконатися, що Git встановлено
-sudo apt-get install -y git
+    # Переконатися, що Git встановлено
+    sudo apt-get install -y git
 
-# Клонувати репозиторій в директорію користувача
-git clone https://github.com/eth-educators/eth-docker $HOME/eth-docker
+    # Клонувати репозиторій в директорію користувача
+    git clone https://github.com/eth-educators/eth-docker $HOME/eth-docker
 
-# Забезпечити резервне копіювання файлів конфігурації
-[ -f $HOME/eth-docker/.env ] && cp $HOME/eth-docker/.env $HOME/eth-docker/.env.backup
+    # Забезпечити резервне копіювання файлів конфігурації
+    [ -f $HOME/eth-docker/.env ] && cp $HOME/eth-docker/.env $HOME/eth-docker/.env.backup
 
-# Змінити власника завантаженої директорії на holesky
-sudo chown -R holesky:holesky $HOME/eth-docker
+    # Змінити власника завантаженої директорії на holesky
+    sudo chown -R holesky:holesky $HOME/eth-docker
 
-# Змінити робочий каталог на eth-docker
-cd $HOME/eth-docker
+    # Змінити робочий каталог на eth-docker
+    cd $HOME/eth-docker
 
-# Запустити ./ethd config
-./ethd config
+    # Запустити ./ethd config
+    ./ethd config
 
-# Редагувати конфігураційні файли
-sed -i -e "s%COMPOSE_FILE=lighthouse-cl-only.yml:geth.yml.*%COMPOSE_FILE=lighthouse-cl-only.yml:geth.yml:el-shared.yml%g" $HOME/eth-docker/.env
-sed -i -e "s%ARCHIVE_NODE=.*%ARCHIVE_NODE=true%g" $HOME/eth-docker/.env
+    # Редагувати конфігураційні файли
+    sed -i -e "s%COMPOSE_FILE=lighthouse-cl-only.yml:geth.yml.*%COMPOSE_FILE=lighthouse-cl-only.yml:geth.yml:el-shared.yml%g" $HOME/eth-docker/.env
+    sed -i -e "s%ARCHIVE_NODE=.*%ARCHIVE_NODE=true%g" $HOME/eth-docker/.env
 
-# Запустити ./ethd up
-./ethd up
-#exit
-echo Для виходу EXIT
+    # Запустити ./ethd up
+    ./ethd up
+    #exit
+    echo Для виходу EXIT
+'
 }
 uninstall() {
 read -r -p "You really want to delete the node? [y/N] " response
