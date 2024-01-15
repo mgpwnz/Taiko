b8509125e9260f48181d3ebe15272c9394348c6b
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-15 19:46:14 +0200

    update

--- holeski.sh
+++ holeski.sh
@@ -32,32 +32,24 @@ else
     sudo usermod -aG sudo holesky
 fi
 
-# Змінити користувача і виконати команди під новим користувачем
+# Змінити користувача та виконати команди під новим користувачем
 sudo -u holesky -H bash <<'EOF'
-    # Встановити Docker
-    touch $HOME/.bash_profile
-	cd $HOME
-	if ! docker --version; then
-		sudo apt update
-		sudo apt upgrade -y
-		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
-		. /etc/*-release
-		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-		sudo apt update
-		sudo apt install docker-ce docker-ce-cli containerd.io -y
-		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
-		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
-	fi
-	if ! docker compose version; then
-		sudo apt update
-		sudo apt upgrade -y
-		sudo apt install wget jq -y
-		local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
-		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
-		sudo chmod +x /usr/bin/docker-compose
-		. $HOME/.bash_profile
-	fi
+    # Встановлення Docker та Docker Compose
+    if ! command -v docker &>/dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    fi
+    
+    if ! command -v docker-compose &>/dev/null; then
+        sudo apt-get install -y wget jq
+        latest_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)
+        sudo wget -O /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/${latest_compose_version}/docker-compose-$(uname -s)-$(uname -m)"
+        sudo chmod +x /usr/local/bin/docker-compose
+    fi
 
     # Переконатися, що Git встановлено
     sudo apt-get install -y git
