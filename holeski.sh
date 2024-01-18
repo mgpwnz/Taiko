c7629838a0355ee2925065082118a8c70dd905f0
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-18 19:06:42 +0200

    update

--- holeski.sh
+++ holeski.sh
@@ -35,35 +35,7 @@ fi
 # Змінити користувача та виконати команди під новим користувачем
 sudo -u holesky -H bash <<'EOF'
     # Встановлення Docker
-        cd $HOME
-
-    if ! command -v docker &> /dev/null; then
-        sudo apt update
-        sudo apt upgrade -y
-        sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
-        . /etc/*-release
-        wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo apt update
-        sudo apt install docker-ce docker-ce-cli containerd.io -y
-        docker_version=$(apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)")
-        sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
-    fi
-
-    if ! command -v docker-compose &> /dev/null; then
-        sudo apt update
-        sudo apt upgrade -y
-        sudo apt install wget jq -y
-        docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
-        sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)"
-        sudo chmod +x /usr/bin/docker-compose
-        . $HOME/.bash_profile
-    fi
-
-    printf "\e[1m\e[32mInstalled\e[0m\n"
-    # Переконатися, що Git встановлено
-    sudo apt-get install -y git
-
+    . <(wget -qO- https://raw.githubusercontent.com/mgpwnz/VS/main/docker.sh)
     # Клонувати репозиторій в директорію користувача
     sudo git clone https://github.com/eth-educators/eth-docker
 
