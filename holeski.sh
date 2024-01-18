ae189f776efdf3d52921d4464f6d98edd37b8b62
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-18 20:03:55 +0200

    update

--- holeski.sh
+++ holeski.sh
@@ -36,17 +36,29 @@ fi
 sudo -u holesky -H bash <<'EOF'
     #docker
     if ! command -v docker &> /dev/null; then
-		sudo apt update
-		sudo apt-get install unattended-upgrades
-		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
-		. /etc/*-release
-		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-		sudo apt update
-		sudo apt install docker-ce docker-ce-cli containerd.io -y
-		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
-		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
-	fi
+        sudo apt-get install unattended-upgrades
+        sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+
+        # Отримання інформації про версію дистрибутиву
+        . /etc/*-release
+
+        # Додавання ключа для Docker
+        wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+        # Додавання репозиторію Docker
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+        # Оновлення списку пакетів
+        sudo apt update
+
+        # Встановлення Docker
+        sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+        # Встановлення конкретної версії Docker
+        docker_version=$(apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)")
+        sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+    fi
+
     # git
     sudo apt-get install -y git
     cd /home/holesky/
