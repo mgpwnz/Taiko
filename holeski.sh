d9164db84c76ee1e7d750ee9484583d94cf5cfa9
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-18 18:37:05 +0200

    update

--- holeski.sh
+++ holeski.sh
@@ -34,8 +34,21 @@ else
 fi
 # Змінити користувача та виконати команди під новим користувачем
 sudo -u holesky -H bash <<'EOF'
-    # Встановлення Docker та Docker Compose
-    . <(wget -qO- https://raw.githubusercontent.com/mgpwnz/VS/main/docker.sh)
+    # Встановлення Docker
+    	cd $HOME
+	if ! command -v docker &> /dev/null; then
+		sudo apt update
+		sudo apt upgrade -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		. /etc/*-release
+		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt update
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
+		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+	fi
+    printf "\e[1m\e[32mInstalled\e[0m\n"
     # Переконатися, що Git встановлено
     apt-get install -y git
 
