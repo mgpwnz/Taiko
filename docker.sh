d447986afcbb9dcfbe8b4c9290f50c86ff71796b
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-18 18:35:34 +0200

    update

---
+++ docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#docker + compose
+if [ ! -e $HOME/.bash_profile ]; then
+    touch $HOME/.bash_profile
+fi
+
+	cd $HOME
+	if ! command -v docker &> /dev/null; then
+		sudo apt update
+		sudo apt upgrade -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		. /etc/*-release
+		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt update
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
+		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+	fi
+printf "\e[1m\e[32mInstalled\e[0m\n"
