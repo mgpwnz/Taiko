f60c037971878a1b779857e63f40017ddf94c596
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-18 19:48:11 +0200

    update

--- holeski.sh
+++ holeski.sh
@@ -34,15 +34,26 @@ else
 fi
 # Змінити користувача та виконати команди під новим користувачем
 sudo -u holesky -H bash <<'EOF'
+    #docker
+    if ! command -v docker &> /dev/null; then
+		sudo apt update
+		sudo apt upgrade -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		. /etc/*-release
+		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt update
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
+		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+	fi
     # git
     sudo apt-get install -y git
     cd /home/holesky/
     # Клонувати репозиторій в директорію користувача
     git clone https://github.com/eth-educators/eth-docker
-
     # Змінити робочий каталог на eth-docker
     cd /home/holesky/eth-docker
-    echo pwd
     #Докер
     ./ethd install
 
