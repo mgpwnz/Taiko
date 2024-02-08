82f43e431b049be599453173ff2336e242a8f32c
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-02-08 21:49:26 +0200

    update

--- taiko6.sh
+++ taiko6.sh
@@ -4,42 +4,43 @@ while true
 do
 # Menu
 PS3='Select an action: '
-#options=("Holesky" "Holesky logs" "Download the components" "Create the configuration" "Run Taiko" "Enable proposer" "Update Taiko" "logs"  "Uninstall" "Exit")
 options=("Download the components" "Create the configuration" "Run Taiko" "Update Taiko" "logs"  "Uninstall" "Exit")
 select opt in "${options[@]}"
                do
                    case $opt in                          
 
-"Holesky")
-#Holesky
-if id "holesky" &>/dev/null; then
-    echo "Користувач holesky вже існує."
-    echo -e "\e[91mПеред запуском перевір чи синхронізувалася твоя нода http://$(wget -qO- eth0.me):3000/\e[0m"
-else
-. <(wget -qO- https://raw.githubusercontent.com/mgpwnz/Taiko/main/holeski.sh)
-echo -e "\e[91mГрафана Holesky  http://$(wget -qO- eth0.me):3000/\e[0m"
-fi
-
-break
-;;
-"Holesky logs")
-docker logs -n 10 -f eth-docker-execution-1
-break
-;;
 "Download the components")
 # Clone repository
 git clone https://github.com/taikoxyz/simple-taiko-node.git
-cd simple-taiko-node
-cp .env.sample .env 
 break
 ;;
 "Create the configuration")
+cd $HOME/simple-taiko-node/
+rm .env 
+cp .env.sample .env 
+# ADD PRIVATE KEY
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+if [ ! $MMP ]; then
+		read -p "Enter Metamask Private Key : " MMP
+		echo 'export MMP='${MMP} >> $HOME/.bash_profile
+        fi
+ . $HOME/.bash_profile
+#PROPOSER CONFIG 
+sed -i -e "s%L1_PROPOSER_PRIVATE_KEY=.*%L1_PROPOSER_PRIVATE_KEY=${MMP}%g" $HOME/simple-taiko-node/.env
+sed -i -e "s%PROVER_ENDPOINTS=.*%PROVER_ENDPOINTS=$link%g" $HOME/simple-taiko-node/.env
+sed -i -e "s%BLOCK_PROPOSAL_FEE=.*%BLOCK_PROPOSAL_FEE=30%g" $HOME/simple-taiko-node/.env
+sed -i -e "s%PROVER_ENDPOINTS=.*%PROVER_ENDPOINTS=http://taiko-a6-prover.zkpool.io:9876%g" $HOME/simple-taiko-node/.env
+#RPC config
 read -r -p "Holesky is local node? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
 sed -i -e "s%L1_ENDPOINT_HTTP=.*%L1_ENDPOINT_HTTP=http://127.0.0.1:8545%g" $HOME/simple-taiko-node/.env
 sed -i -e "s%L1_ENDPOINT_WS=.*%L1_ENDPOINT_WS=ws://`wget -qO- eth0.me`:8546%g" $HOME/simple-taiko-node/.env
 sed -i -e "s%PORT_GRAFANA=3001%PORT_GRAFANA=3002%g" $HOME/simple-taiko-node/.env
+
             ;;
     *)
         read -p "Enter ip ADDRESS: " HL
@@ -53,7 +54,7 @@ esac
 break
 ;;
 "Run Taiko")
-read -r -p "Запустити ноду? [y/N] " response
+read -r -p "Run node? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
 cd $HOME/simple-taiko-node/
@@ -61,7 +62,9 @@ docker compose up -d
 docker compose logs -f
             ;;
     *)
-        echo Відміна!
+        cd $HOME/simple-taiko-node/
+        docker compose up -d 
+        docker compose logs -f
         break
         ;;
 esac
@@ -74,6 +77,11 @@ git pull
 sleep 2
 rm .env 
 cp .env.sample .env 
+#PROPOSER CONFIG 
+sed -i -e "s%L1_PROPOSER_PRIVATE_KEY=.*%L1_PROPOSER_PRIVATE_KEY=${MMP}%g" $HOME/simple-taiko-node/.env
+sed -i -e "s%PROVER_ENDPOINTS=.*%PROVER_ENDPOINTS=$link%g" $HOME/simple-taiko-node/.env
+sed -i -e "s%BLOCK_PROPOSAL_FEE=.*%BLOCK_PROPOSAL_FEE=30%g" $HOME/simple-taiko-node/.env
+sed -i -e "s%PROVER_ENDPOINTS=.*%PROVER_ENDPOINTS=http://taiko-a6-prover.zkpool.io:9876%g" $HOME/simple-taiko-node/.env
 read -r -p "Holesky is local node? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
@@ -89,19 +97,6 @@ sed -i -e "s%PORT_GRAFANA=3001%PORT_GRAFANA=3002%g" $HOME/simple-taiko-node/.env
         break
         ;;
 esac
-#sleep 3
-#read -r -p "Run proposer? [y/N] " response
-#case "$response" in
-#    [yY][eE][sS]|[yY]) 
-#    sed -i -e "s%ENABLE_PROPOSER=false%ENABLE_PROPOSER=true%g" $HOME/simple-taiko-node/.env
-#    sed -i -e "s%PROVER_ENDPOINTS=.*%PROVER_ENDPOINTS=$link%g" $HOME/simple-taiko-node/.env
-#            ;;
-#    *)
-#        echo Running
-#        break
-#        ;;
-#esac
-#docker stop
 cd $HOME/simple-taiko-node && docker compose down
 #docker start
 docker compose up -d
@@ -111,30 +106,6 @@ break
 
 "logs")
 docker compose -f $HOME/simple-taiko-node/docker-compose.yml logs -f --tail 250
-break
-;;
-"Enable proposer")
-bash_profile=$HOME/.bash_profile
-if [ -f "$bash_profile" ]; then
-    . $HOME/.bash_profile
-fi
-if [ ! $MMA ]; then
-		read -p "Enter Metamask address : " MMA
-		echo 'export MMA='${MMA} >> $HOME/.bash_profile
-        fi
-if [ ! $MMP ]; then
-		read -p "Enter Metamask Private Key : " MMP
-		echo 'export MMP='${MMP} >> $HOME/.bash_profile
-        fi
- . $HOME/.bash_profile
-sed -i -e "s%L1_PROPOSER_PRIVATE_KEY=.*%L1_PROPOSER_PRIVATE_KEY=${MMP}%g" $HOME/simple-taiko-node/.env
-sed -i -e "s%L2_SUGGESTED_FEE_RECIPIENT=.*%L2_SUGGESTED_FEE_RECIPIENT=${MMA}%g" $HOME/simple-taiko-node/.env
-sed -i -e "s%ENABLE_PROPOSER=false%ENABLE_PROPOSER=true%g" $HOME/simple-taiko-node/.env
-sed -i -e "s%PROVER_ENDPOINTS=.*%PROVER_ENDPOINTS=$link%g" $HOME/simple-taiko-node/.env
-cd $HOME/simple-taiko-node
-docker compose up -d
-sleep 5
-docker compose logs -f
 break
 ;;
 "Uninstall")
